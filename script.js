0e616cc645e818a1777c78bf6a6eaea3a011503d
Subhadip Das <143736876+SubhadipLol@users.noreply.github.com>
2024-08-08 22:50:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector(".fa-github").onclick=function(){
+    window.location.href="https://github.com/SubhadipLol";
+}
+document.querySelector(".fa-linkedin-in").onclick=function(){
+    window.location.href="https://www.linkedin.com/in/subhadip-das-35543a293/";
+}
